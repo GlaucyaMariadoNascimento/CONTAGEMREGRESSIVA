a11f285c738e9e15644da025002bce155526941c
Savioli Pri <stobbeglaucya@gmail.com>
2021-08-28 13:41:37 -0300

    Adiciona solião exercicio contagem regressiva

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CONTAGEMREGRESSIVA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Preparar...");
+            Console.ReadKey();
+            Console.WriteLine("Apontar");
+            Console.ReadKey();
+            Console.WriteLine("fogo!");
+            Console.ReadKey();
+        }
+    }
+}
